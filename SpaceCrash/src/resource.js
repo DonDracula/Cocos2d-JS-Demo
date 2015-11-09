feb4f9b3fb055efb72ca03ba2c7027c821631606
Liuyanhao <DonDracula@users.noreply.github.com>
2015-11-09 17:55:45 +0900

    Ship crash Demo
    a game  that one should control the ship avoid crash with the arstroid

---
+++ SpaceCrash/src/resource.js
@@ -0,0 +1,11 @@
+var res = {
+    background_jpg : "res/background.jpg",
+    ship_png : "res/ship.png",
+    particle_png : "res/particle.png",
+    asteroid_png : "res/asteroid.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
